20cee166383c15694246f66820db5939896c6b86
Tony Rasa <t_rasa@wargaming.net>
2016-04-08 12:03:11 -0700

    get wallets

--- main.go
+++ main.go
@@ -6,23 +6,43 @@ import (
 	"html"
 	"log"
 	"net/http"
+	"encoding/json"
 )
+
+type Wallet struct {
+	CurrencyType string	`json:"currency_type"`
+	Amount int	`json:"amount"`
+}
+type Wallets []Wallet
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", Index)
-	router.HandleFunc("/wallets", Wallets)
+	router.HandleFunc("/wallets", GetWallets)
+	router.HandleFunc("/wallets/{currencyType}", WalletCurrency)
+
     log.Println("Listening on 8080...")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
-    log.Printf("Index Request: %s", r)
+    log.Printf("Index Request")
 	fmt.Fprintf(w, "you are at %q", html.EscapeString(r.URL.Path))
 }
 
-func Wallets(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Get Wallets: %s", r)
+func GetWallets(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Get Wallets")
 	// TODO get wallets
-	fmt.Fprint(w, "{}");
+	wallets := Wallets {
+		Wallet {  CurrencyType: "gold", Amount: 123},
+		Wallet {  CurrencyType: "silver", Amount: 5555},
+	}
+	json.NewEncoder(w).Encode(wallets)
+}
+
+func WalletCurrency(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	currencyType := vars["currencyType"]
+	log.Printf("Get Wallet Currency: %s", currencyType)
+	fmt.Fprintf(w, "currency type %s", currencyType)
 }
