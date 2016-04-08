6bddbe6901c882f670f3e6a63d943199f36a6540
Tony Rasa <t_rasa@wargaming.net>
2016-04-08 12:50:12 -0700

    split router from routes

--- handlers.go
+++ handlers.go
@@ -17,7 +17,11 @@ func GetWallets(w http.ResponseWriter, r *http.Request) {
 		Wallet {  CurrencyType: "gold", Amount: 123},
 		Wallet {  CurrencyType: "silver", Amount: 5555},
 	}
-	json.NewEncoder(w).Encode(wallets)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(wallets); err != nil {
+		panic(err)
+	}
 }
 
 func WalletCurrency(w http.ResponseWriter, r *http.Request) {
--- routes.go
+++ routes.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
 	"net/http"
 )
 
@@ -12,24 +11,6 @@ type Route struct {
 	HandlerFunc http.HandlerFunc
 }
 type Routes []Route
-
-func NewRouter() *mux.Router {
-
-	router := mux.NewRouter().StrictSlash(true)
-	for _, route := range routes {
-		var handler http.Handler
-		handler = route.HandlerFunc
-		handler = Logger(handler, route.Name)
-
-		router.
-			Methods(route.Method).
-			Path(route.Pattern).
-			Name(route.Name).
-			Handler(handler)
-	}
-
-	return router
-}
 
 var routes = Routes{
 	Route{
